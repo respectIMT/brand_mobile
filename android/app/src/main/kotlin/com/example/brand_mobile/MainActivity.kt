4e732926f88ab3493eca21f8816f24083196ec29
Muhsinjon Ibrohimov <muhsinjonibrohimov2002@gmail.com>
2023-11-09 17:55:12 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/brand_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.brand_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_screen.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class MAinScreen extends StatefulWidget {
+  const MAinScreen({super.key});
+
+  @override
+  State<MAinScreen> createState() => _MAinScreenState();
+}
+
+class _MAinScreenState extends State<MAinScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      
+      
+    );
+  }
+}
